ca35ae82f77276ed5ff629974a47bc9253f3a856
si180@yahoo.com <si180@yahoo.com>
2018-08-05 08:39:56 -0400

    info

---
+++ push.bat
@@ -0,0 +1,5 @@
+git config --global credential.helper 'cache --timeout=28800'
+git add .
+git commit -m "info"
+git remote add origin https://github.com/sachkosm/react-boilerplate-lists.git
+git push -f -u origin master
